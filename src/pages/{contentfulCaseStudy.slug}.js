32157e59918fa292ee32539021de1af8174de7a9
Mike Henderson <services@michaelrhenderson.com>
2022-03-16 18:02:22 -0700

    adjusted styles in case study format
    Added a css module for case study pages specifically.

--- src/pages/{contentfulCaseStudy.slug}.js
+++ src/pages/{contentfulCaseStudy.slug}.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Layout } from '../components/layout/layout.js'
 import { RichText } from '../components/richText/richText.js'
 import {graphql} from "gatsby"
+import * as Styles from '../styles/caseStudyPage.module.scss'
+
 
 /* import {documentToReactComponents} from '@contentful/rich-text-react-renderer' */
 
@@ -10,16 +12,20 @@ export default function ContentfulCaseStudy(props) {
 
   return (
     <Layout>
-    <span className="caseStudySubHeading">Case Study: {props.data.contentfulCaseStudy.role}</span>
-    <h2>{props.data.contentfulCaseStudy.caseStudyTitle}</h2>
-      {!!props.data.contentfulCaseStudy.caseStudyContent && (
-        <RichText
-            references={
-                props.data.contentfulCaseStudy.caseStudyContent.references
-            }
-            raw={props.data.contentfulCaseStudy.caseStudyContent.raw}
-        />
-      )}
+    <div className={Styles.caseStudyWrapper}>
+      <div className={Styles.caseStudySubHeading}>
+        <span>Case Study: {props.data.contentfulCaseStudy.role}</span>
+      </div>
+      <h2>{props.data.contentfulCaseStudy.caseStudyTitle}</h2>
+        {!!props.data.contentfulCaseStudy.caseStudyContent && (
+          <RichText
+              references={
+                  props.data.contentfulCaseStudy.caseStudyContent.references
+              }
+              raw={props.data.contentfulCaseStudy.caseStudyContent.raw}
+          />
+        )}
+    </div>
     </Layout>
   );
 }
